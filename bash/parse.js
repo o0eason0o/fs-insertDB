52cd82c9624339f077e5fa7f7ac13d375d397106
o0eason0o <ylu23@syr.edu>
2016-03-28 15:18:12 -0400

    add promises

--- bash/parse.js
+++ bash/parse.js
@@ -1,42 +1,79 @@
 var fs = require('fs');
-var db = require('./db');
+var _ = require('lodash');
 
-var interviews = [];
+function parseLine(l, interview, interviews) {
+    var line = l.trim();
+    if (line.startsWith('---')) {
+        //TODO, add previous interview info to interviews list
+        if (!!interview.Client) interviews.push(interview);
+        return {};
+    }
 
-fs.readFile(__dirname + '/data/Barclays.txt', 'utf8', function(err, cnt) {
-    console.log(err, cnt.length);
-    var lines = cnt.split('\n');
-    var interview = {};
-    lines.forEach(function(l) {
-        var line = l.trim();
-        if (line.startsWith('---')) {
-            //TODO, add previous interview info to interviews list
-            if (!!interview.Client) interviews.push(interview);
-            interview = {};
-            return;
+    var isHeader = false;
+
+    ['Client', 'Candidate', 'Date', 'Type'].forEach(function(k) {
+        if (line.startsWith(k)) {
+            interview[k] = line.split(':')[1].trim();
         }
+    });
 
-        var isHeader = false;
-
-        ['Client', 'Candidate', 'Data', 'Type'].forEach(function(k) {
-            if (line.startsWith(k)) {
-                interview[k] = line.split(':')[1].trim();
-            }
-        });
-
-        if (!isHeader) {
-            var matchRes = /^\d+\.\s*(.*)/.exec(line.trim());
-            if (!matchRes) return;
+    if (!isHeader) {
+        var matchRes = /^\d+\.\s*(.*)/.exec(line.trim());
+        if (!!matchRes) {
             if (!interview.questions) interview.questions = [];
             interview.questions.push(matchRes[1]);
         }
-    });
-    if (!!interview.Client) interviews.push(interview);
+    }
+    return interview;
+}
 
-    db.saveInterviews(interviews)
-        .then(function(info){
-            console.log('after save', info);
-            return db.dbConn.then(function(db){return db.close();});
-        })
-        .catch(console.log);
-})
+function parseFile(file) {
+    var interviews = [];
+    return new Promise(function(resolve, reject) {
+        fs.readFile(file, 'utf8', function(err, cnt) {
+            console.log(err, cnt.length);
+            if (err) {
+                reject(err);
+                return;
+            }
+            var lines = cnt.split('\n');
+            var interview = {};
+            lines.forEach(function(l) {
+                interview = parseLine(l, interview, interviews)
+            });
+            if (!!interview.Client) interviews.push(interview);
+            resolve(interviews);
+
+        });
+    });
+}
+
+function parseDir(dir) {
+    var promises = [];
+
+    return new Promise(function(resolve, reject) {
+        fs.readdir(dir, function(err, files) {
+            console.log('read dir', err, files.length);
+            if (err) return;
+            var promises = _.map(files, function(file) {
+                var filePath = dir + '/' + file;
+                var fsStat = fs.statSync(filePath);
+                if (fsStat.isDirectory()) return parseDir(filePath);
+                else if (fsStat.isFile() && file.endsWith('.txt'))
+                    return parseFile(filePath);
+                else console.log('ignore', filePath);
+            });
+            Promise.all(promises).then(function(interviewses) {
+                resolve(_.reduce(interviewses, function(interviews, item) {
+                    return interviews.concat(item)
+                }, []));
+            });
+            console.log('read dir finish', dir);
+        });
+    })
+}
+
+module.exports = {
+    parseFile: parseFile,
+    parseDir: parseDir
+}
